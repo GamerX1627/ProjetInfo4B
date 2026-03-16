715428eda90d069790dcd04749e9b429b1b69eef
GamerX1627 <ishigi667@gmail.com>
2026-03-16 03:08:43 +0100

    lodeRunner plateau fini2

--- loderunner/src/main/java/loderunner/utils/LevelLoader.java
+++ loderunner/src/main/java/loderunner/utils/LevelLoader.java
@@ -1,5 +1,52 @@
 package loderunner.utils;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import loderunner.model.Case;
+import loderunner.model.Plateau;
+
 public class LevelLoader {
-    
+    public static Plateau loadMap (String cheminFichier){ // méthode qui permet de charger le plateau à partir d'un fichier texte.
+       try (BufferedReader br = new BufferedReader(new FileReader(cheminFichier))) {
+            String ligne;
+            ligne= br.readLine();
+            String dim[]=ligne.split(" ");
+            int largeur=Integer.parseInt(dim[0]);
+            int hauteur=Integer.parseInt(dim[1]);
+            Plateau P=new Plateau(largeur, hauteur);
+            for(int i=0; i<hauteur;i++){
+                ligne=br.readLine();
+                for(int j=0; j<largeur; j++){
+                    switch (ligne.charAt(j)) {
+                        case '#': { // ici, on se sert du # dans le fichier texte pour indiquer au LevelLoader que c'est un mur
+                            P.setCase(i, j, Case.MUR);break;
+                        }
+                        case 'H': { //ici, on se sert du H pour indiquer que c'est une echelle
+                            P.setCase(i, j, Case.ECHELLE);break;
+                        }
+                        case '-': { // on se sert du - pour indiquer que c'est une passerelle 
+                            P.setCase(i, j, Case.PASSERELLE);break;
+                        }
+                        case ' ': { // on se sert de l'espace pour indiquer que c'est le vide
+                            P.setCase(i, j, Case.VIDE);break;
+                        }
+                        case '£': { // on se sert du £ pour indiquer que c'est un lingot d'or
+                            P.setCase(i, j, Case.LINGOT);break;
+                        }
+                        case 'S':{ // on se sert du S pour indiquer la sortie
+                            P.setCase(i, j, Case.SORTIE);break;
+                        }
+                        case 'O':{ // on se sert du O pour indiquer un trou
+                            P.setCase(i, j, Case.TROU);
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return P;
+    }
 }

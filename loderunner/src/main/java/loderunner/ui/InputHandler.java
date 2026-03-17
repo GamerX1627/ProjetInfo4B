f901b00dcddf55510a487fe66e622445703ddc85
GamerX1627 <ishigi667@gmail.com>
2026-03-17 18:19:07 +0100

    lodeRunner LevelLoader fini

--- loderunner/src/main/java/loderunner/ui/InputHandler.java
+++ loderunner/src/main/java/loderunner/ui/InputHandler.java
@@ -1,5 +1,34 @@
 package loderunner.ui;
 
-public class InputHandler {
-    
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import loderunner.utils.Direction;
+
+public class InputHandler implements KeyListener {
+    private Direction directionCourante = Direction.AUCUNE;
+
+    public Direction getDirection() {
+        return directionCourante;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_UP:    directionCourante = Direction.HAUT; break;
+            case KeyEvent.VK_DOWN:  directionCourante = Direction.BAS; break;
+            case KeyEvent.VK_LEFT:  directionCourante = Direction.GAUCHE; break;
+            case KeyEvent.VK_RIGHT: directionCourante = Direction.DROITE; break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        // lorsque la touche est relâchée, on ramène la direction à aucune.
+        directionCourante = Direction.AUCUNE;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
 }

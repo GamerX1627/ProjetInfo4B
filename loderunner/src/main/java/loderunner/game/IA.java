714b4e6b3f345a09af38f0234369f5eaa80685e0
GamerX1627 <ishigi667@gmail.com>
2026-04-08 17:26:36 +0200

    la fenêtre marche, les animations aussi

--- loderunner/src/main/java/loderunner/game/IA.java
+++ loderunner/src/main/java/loderunner/game/IA.java
@@ -1,5 +1,5 @@
 package loderunner.game;
-//Classe qui se permet de calculer le meilleur chemin pour l'IA afin qu'elle se dirige vers le joueur.
+//Classe qui se permet de calculer le meilleur chemin pour l'IA afin qu'elle se dirige vers le joueur. à faire demain
 public class IA {
     
 }
